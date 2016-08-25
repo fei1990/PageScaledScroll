0ae16d94c5c7c32c2981189423b2253147ec4315
sunAndStar <f.wang1990@hotmail.com>
2016-08-25 15:47:00 +0800

    first commit

---
+++ ScaledPageView/ScaledPageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScaledPageView
+//
+//  Created by wangfei on 16/8/25.
+//  Copyright © 2016年 fei.wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
